bbafa5bfc246effe2f7894793fc99a47d7de42e5
João Neto <joao.neto@sprinklr.com>
2015-11-06 17:17:00 -0200

    ref() eslinter

--- src/Hook.jsx
+++ src/Hook.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import Region from './Region.jsx';
 
 export default class Hook extends React.Component {
+  static propTypes = {
+    where: React.PropTypes.any,
+    to: React.PropTypes.any,
+    children: React.PropTypes.element,
+  }
+
   render() {
     let myRoute = Region.getMyportion(
     	this.props.where.location,
@@ -12,6 +18,7 @@ export default class Hook extends React.Component {
     if (location.hash.indexOf('#') !== -1){
     	myRoute.diff = '#' + myRoute.diff
     }
+
     return <a href={myRoute.diff}>{this.props.children}</a>
   }
 };
